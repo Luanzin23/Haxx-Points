a90a01449ae6bc145290c8cd885b7f51f3ef23bd
Haxx FiveM <87834732+Luanzin23@users.noreply.github.com>
2022-01-29 02:41:57 -0300

    Update haxxcz.lua

--- haxxcz.lua
+++ haxxcz.lua
@@ -1,17 +1,5 @@
 local PointsVisible = false
 
-function DrawGenericText(text)
-	SetTextColour(186, 186, 186, 255)
-	SetTextFont(7)
-	SetTextScale(0.378, 0.378)
-	SetTextWrap(0.0, 1.0)
-	SetTextCentre(false)
-	SetTextDropshadow(0, 0, 0, 0, 255)
-	SetTextEdge(1, 0, 0, 0, 205)
-	SetTextEntry("STRING")
-	AddTextComponentString(text)
-	DrawText(0.40, 0.00)
-end
 
 Citizen.CreateThread(function()
     while true do
@@ -26,4 +14,3 @@ Citizen.CreateThread(function()
 		Citizen.Wait(sleepThread)
 	end
 end)
-
